07846e6a9c12b22b0ddf6cfe141b3e94b8156895
Vu Ngoc Anh <hoathong99@gmail.com>
2025-02-22 15:35:24 +0700

    update resume screen

--- src/app/components/resume/resume.component.ts
+++ src/app/components/resume/resume.component.ts
@@ -4,8 +4,25 @@ import { Component } from '@angular/core';
   selector: 'app-resume',
   imports: [],
   templateUrl: './resume.component.html',
-  styleUrl: './resume.component.css'
+  styleUrl: './resume.component.css',
 })
 export class ResumeComponent {
-
+  Localize: string = 'EN';
+  switchLanguage(input: string) {
+    console.log("trigger switch language");
+    switch (input) {
+      case 'EN': {
+        this.Localize = 'EN';
+        break;
+      }
+      case 'VN': {
+        this.Localize = 'VN';
+        break;
+      }
+      default: {
+        this.Localize = 'EN';
+        break;
+      }
+    }
+  }
 }
